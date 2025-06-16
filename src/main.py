9d4677eab45e572c6e8f9572d7522e4d4171f988
Jamie Harris <jamie@harrishomesite.com>
2025-06-16 17:59:01 +0100

    working proportion training network

--- src/main.py
+++ src/main.py
@@ -1,72 +1,82 @@
-import yaml
-import torch
-from torch.utils.data import DataLoader
-import numpy as np
 import pandas as pd
-import random
-import os
-from sklearn.model_selection import train_test_split
+import yaml
+from pathlib import Path
+from torch.utils.data import DataLoader
+import torch.nn as nn
 
-from src.tools.data_utils import ReportingDataset
-from src.tools.train_utils import SubsetSampler, SubsetRandomSampler
-from src.tools.train_utils import train, EarlyStopper, 
-from src.tools.NowcastPNN import NowcastPNN
+from tools.data_utils import *
+from tools.proportion_matrix_net import ProportionMatrixNet
+from tools.train_utils import EarlyStopper, train
+
+
+
+# Get the directory of the current script
+project_dir = Path(__file__).resolve().parents[1]
 
 # Load config vars
-with open('config.yaml', 'r') as f:
+with open( project_dir / "config.yaml", 'r') as f:
     config = yaml.safe_load(f)
 
-SEED = config['training']['seed']
+MAX_DELAY = config['model']['max_delay']
 BATCH_SIZE = config['training']['batch_size']
 
-PAST_UNITS = config['model']['past_units']
-MAX_DELAY = config['model']['max_delay']
-STATE = config['model']['state']
-DEVICE = config['training']['device']
+reporting_delay_data_path = create_reporting_delay_matrix(
+    start_year = 2013,
+    end_year = 2020,
+    max_delay = MAX_DELAY,
+    input_data_filename = 'DENGSP.csv',
+    output_data_filename = 'dengue-sp-reporting-delay.csv'
+)
 
-# set_seeds()
-torch.manual_seed(SEED)
-np.random.seed(SEED)
-random.seed(SEED)
-torch.cuda.manual_seed(SEED)
-os.environ["PYTHONHASHSEED"] = str(SEED)
-torch.backends.cudnn.deterministic = True
-
-# Load Dengue Data
-path = "data/dengue-sp-reporting-delay.csv"
-dengdf = pd.read_csv(path, index_col=0)
-dengdf = np.array(dengdf.values, dtype = np.float32)
-max_val = dengdf.max(axis = 1).max(axis=0)
-dataset = ReportingDataset(dengdf, max_val=max_val, triangle=True, past_units=PAST_UNITS, max_delay=MAX_DELAY, future_obs=0, vector_y = False, dow = dow, return_number_obs = 0)
-
-# Train/Test split
-weeks = False
-dow = False
-random_split = True
-
-all_idcs = range(dataset.__len__())
-train_idcs, test_idcs = train_test_split(all_idcs, test_size=0.25, shuffle=True, random_state=SEED)
-train_idcs, val_idcs = train_test_split(train_idcs, test_size=0.25, shuffle=True, random_state=SEED)
-val_batch_size, test_batch_size = len(val_idcs), len(test_idcs)
-
-g = torch.Generator()
-g.manual_seed(SEED)
-
-train_sampler = SubsetRandomSampler(train_idcs, generator=g)
-val_sampler = SubsetRandomSampler(val_idcs)
-test_sampler = SubsetSampler(test_idcs)
-
-train_loader = DataLoader(dataset, batch_size=BATCH_SIZE, sampler=train_sampler)
-val_loader = DataLoader(dataset, batch_size=val_batch_size, sampler=val_sampler, shuffle=False)
-test_loader = DataLoader(dataset, batch_size=test_batch_size, sampler=test_sampler, shuffle=False)
+reporting_delay_prop_data_path = create_reporting_proportion_matrix(
+    max_delay = MAX_DELAY,
+    input_data_filename = 'dengue-sp-reporting-delay.csv',
+    output_data_filename = 'dengue-sp-reporting-delay-proportion.csv'
+)
 
 
-# Train model
-early_stopper = EarlyStopper(patience=30, past_units=PAST_UNITS, max_delay=MAX_DELAY, weeks=weeks, future_obs=0, random_split=random_split, dow = dow)
-nowcast_pnn = NowcastPNN(past_units=PAST_UNITS, max_delay=MAX_DELAY, conv_channels=[16, 1], hidden_units=[16, 8], dropout_probs=[0.3, 0.1])
-train(nowcast_pnn, num_epochs=200, train_loader=train_loader, val_loader=val_loader, early_stopper=early_stopper, loss_fct="nll", device = DEVICE, dow = dow)
+# Read in raw data
+dengdf = pd.read_csv(reporting_delay_prop_data_path, index_col=0)
 
-# Load best set of weights on test/validation set
-nowcast_pnn.load_state_dict(torch.load(f"src/outputs/weights/weights-{PAST_UNITS}-{MAX_DELAY}-{'week' if weeks else 'day'}-fut0{'-rec' if not random_split else ''}{'-dow' if dow else ''}"))
+# Drop total counts
+propdf = dengdf.drop(["y"], axis=1)
 
+# Drop last max_delay days as they are incomplete
+propdf = propdf.iloc[0: -MAX_DELAY]
+
+# Split into train, val, test datasets
+train_prop, val_prop, test_prop = 0.7, 0.15, 0.15
+
+# First max_delay days are not usable
+n_usable_obs = len(propdf) - MAX_DELAY
+
+# Split the number of usable obs over the 3 datasets
+train_end_idx = MAX_DELAY + int(train_prop*n_usable_obs)
+val_end_idx = train_end_idx + int(val_prop*n_usable_obs)
+
+train_df = propdf.iloc[MAX_DELAY: train_end_idx]
+val_df = propdf.iloc[train_end_idx - MAX_DELAY : val_end_idx]
+test_df = propdf.iloc[val_end_idx - MAX_DELAY : ]
+
+# Create PyTorch dataset objects
+train_dataset = ReportingDataset(train_df, max_delay=MAX_DELAY)
+val_dataset   = ReportingDataset(val_df,   max_delay=MAX_DELAY)
+test_dataset  = ReportingDataset(test_df,  max_delay=MAX_DELAY)
+
+# Create PyTorch Dataloaders without shuffle since want to keep temporal relationships
+train_loader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=False)
+val_loader = DataLoader(val_dataset, batch_size=BATCH_SIZE, shuffle=False)
+test_loader = DataLoader(test_dataset, batch_size=BATCH_SIZE, shuffle=False)
+
+# Train model 
+early_stopper = EarlyStopper(patience=config['training']['patience'], max_delay=MAX_DELAY)
+model = ProportionMatrixNet(max_delay=MAX_DELAY)
+optimizer = torch.optim.Adam(model.parameters(), lr=config['training']['adam_learning_rate'])
+loss_fn = nn.MSELoss()
+device = torch.device(config['training']['device'])
+
+train(model, early_stopper, train_loader, val_loader, optimizer, loss_fn, device, num_epochs=20)
+
+# Load best set of weights on validation set
+model.load_state_dict(torch.load(project_dir / "src" / "outputs" / "weights" / f"weights_max_delay_{MAX_DELAY}"))
 
---
+++ src/tools/proportion_matrix_net.py
@@ -0,0 +1,54 @@
+import torch.nn as nn
+import torch.nn.functional as F
+
+class ProportionMatrixNet(nn.Module):
+    def __init__(self, max_delay):
+        super().__init__()
+        self.max_delay = max_delay
+
+        # Multihead Attention (treat each row as a sequence)
+        self.attn = nn.MultiheadAttention(embed_dim=self.max_delay, num_heads=1, batch_first=True)
+
+        # FC Block
+        self.fc1 = nn.Linear(self.max_delay, self.max_delay)
+        self.fc2 = nn.Linear(self.max_delay, self.max_delay)
+
+        # Conv Block
+        self.conv1 = nn.Conv2d(1, 16, kernel_size=3, padding=1)
+        self.bn1 = nn.BatchNorm2d(16)
+        self.conv2 = nn.Conv2d(16, 16, kernel_size=3, padding=1)
+        self.bn2 = nn.BatchNorm2d(16)
+        self.conv3 = nn.Conv2d(16, 1, kernel_size=1)
+
+        # SiLu activation
+        self.silu = nn.SiLU()
+
+        # Dropout to prevent over fitting
+        self.drop1 = nn.Dropout(p=0.15)
+        self.drop2 = nn.Dropout(p=0.1)
+
+
+    def forward(self, x):
+        x_add = x.clone()
+        
+        # Attention across rows
+        x, _ = self.attn(x, x, x)  # shape: [B, D, D]
+        x = self.drop1(x)
+
+        # FF on rows
+        x = self.silu(self.fc1(x))
+        x = self.fc2(x)
+        x = self.drop2(x)
+
+        # Residual connection
+        x = x + x_add
+
+        # Conv layers treating as if image
+        x = x.unsqueeze(1)
+        x = self.silu(self.bn1(self.conv1(x)))
+        x = self.silu(self.bn2(self.conv2(x)))
+        x = self.conv3(x)
+        x = x.squeeze(1)
+
+        return F.softmax(x, dim=-1)
+
